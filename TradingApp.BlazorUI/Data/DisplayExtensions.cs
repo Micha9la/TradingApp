b3b0c6db9c1e10c3d419d31063503f3b64f8b668
Micha9la <m.hunarwindhager@gmail.com>
2025-08-12 15:21:20 +0200

    created extension method to show a value or an empty cell in the table

---
+++ TradingApp.BlazorUI/Data/DisplayExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TradingApp.BlazorUI.Data
+{
+    public static class DisplayExtensions
+    {
+        public static string ValueOrEmpty(this object value)
+        {
+            if (value == null) return string.Empty;
+            if (value is string s) return string.IsNullOrWhiteSpace(s) ? string.Empty : s;
+            if (value.Equals(0)) return string.Empty;
+            if (value.Equals(0.0f) || value.Equals(0.0d) || value.Equals(0.0m)) return string.Empty;
+            if (value.GetType().IsEnum && value.Equals(Activator.CreateInstance(value.GetType()))) return string.Empty;
+
+            return value.ToString();
+        }
+    }
+}
