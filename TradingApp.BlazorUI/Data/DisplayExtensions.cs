d971f49fa65424fc6f71518740c7818c46a7ddf4
Micha9la <m.hunarwindhager@gmail.com>
2025-08-13 11:20:00 +0200

    changed extension method ValueOrEmpty to call the same method on all value types including enums

--- TradingApp.BlazorUI/Data/DisplayExtensions.cs
+++ TradingApp.BlazorUI/Data/DisplayExtensions.cs
@@ -1,16 +1,67 @@
 ﻿namespace TradingApp.BlazorUI.Data
 {
-    public static class DisplayExtensions
+    public static class NumericExtensions
     {
-        public static string ValueOrEmpty(this object value)
+        public static string ValueOrEmpty<TEnum>(this TEnum value) where TEnum : struct, Enum
         {
-            if (value == null) return string.Empty;
-            if (value is string s) return string.IsNullOrWhiteSpace(s) ? string.Empty : s;
-            if (value.Equals(0)) return string.Empty;
-            if (value.Equals(0.0f) || value.Equals(0.0d) || value.Equals(0.0m)) return string.Empty;
-            if (value.GetType().IsEnum && value.Equals(Activator.CreateInstance(value.GetType()))) return string.Empty;
+            // If value is the default (usually 0), return empty
+            if (EqualityComparer<TEnum>.Default.Equals(value, default))//checks if the value is equal to the enum’s default.
+                return string.Empty;
 
-            return value.ToString();
+            return value.ToString();// value.ToString = name of enum
+        }
+        public static string ValueOrEmpty(this int x)
+        {
+            if (x != 0)
+                return x.ToString();
+            else
+                return string.Empty;
+        }
+
+        public static string ValueOrEmpty(this float x)
+        {
+            if (x != 0)
+                return x.ToString();
+            else
+                return string.Empty;
+        }
+
+        public static string ValueOrEmpty(this decimal x)
+        {
+            if (x != 0)
+                return x.ToString();
+            else
+                return string.Empty;
+        }
+        public static string ValueOrEmpty(this decimal? x)//nullable value type
+        {
+            if (x.HasValue && x.Value != 0)
+                return x.Value.ToString();
+            else
+                return string.Empty;
+        }
+        public static string ValueOrEmpty(this int? x)
+        {
+            if (x.HasValue && x.Value != 0)
+                return x.Value.ToString();
+            else
+                return string.Empty;
+        }
+
+        public static string ValueOrEmpty(this float? x)
+        {
+            if (x.HasValue && x.Value != 0)
+                return x.Value.ToString();
+            else
+                return string.Empty;
+        }
+
+        public static string ValueOrEmpty(this string x)
+        {
+            if (!string.IsNullOrWhiteSpace(x))
+                return x;
+            else
+                return string.Empty;
         }
     }
 }
